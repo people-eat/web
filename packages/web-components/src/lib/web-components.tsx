234485e789fa1aee8473b8fb7dab71661d0b0f45
Cem Yilmaz <42110001+CemYil03@users.noreply.github.com>
2023-08-10 07:29:12 +0200

    feat(web-components): setup material ui

--- packages/web-components/src/lib/web-components.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './web-utils.module.css';
-
-export function WebComponents() {
-    return (
-        <div className={styles['container']}>
-            <h1>Welcome to Web Components!</h1>
-        </div>
-    );
-}
---
+++ packages/web-components/src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme, type Theme } from '@mui/material';
+
+export const theme: Theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FF6433',
+            contrastText: 'white',
+        },
+    },
+});
